176407b79aa15601ed907257043e4242af769c1a
Sandhya <sandhyarana07610761@gmail.com>
2025-07-16 22:55:00 +0530

    homePage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,42 @@
-import React from "react";
-import "../styles/Header.css";
+// import React from "react";
+// import "../styles/Header.css";
 
-function Header() {
-  return (
-    <header className="header">
-      <div className="header-container">
-        <div className="logo">JobFinder</div>
-        <input type="text" placeholder="Search jobs" className="search-box" />
-        <div className="search-login">
-          {/* <input type="text" placeholder="Search jobs" className="search-box" /> */}
-          <button className="btn login">Login</button>
-          <button className="btn register">Register</button>
-        </div>
-      </div>
-    </header>
-  );
+// function Header() {
+//   return (
+//     <header className="header">
+//       <div className="header-container">
+//         <div className="logo">JobFinder</div>
+//         {/* <input type="text" placeholder="Search jobs" className="search-box" /> */}
+//         <div className="search-login">
+//           <input type="text" placeholder="Search jobs" className="search-box" />
+//           <button className="btn login">Login</button>
+//           <button className="btn register">Register</button>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default Header;
+ import "../styles/Header.css"
+
+export default function Header(){
+    return (
+        <header>
+            <div className="header-container">
+                <div className="logo"><h3>JOBS</h3></div>
+                <div className="header-options">
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Jobs</a>
+                    <a href="">Candidates</a>
+                    <a href="">Contact Us</a>
+                </div>
+                <div className="accounts-btn">
+                    <button>Sign Up</button>
+                    <button>Sign In</button>
+                </div>
+            </div>
+        </header>
+    )
 }
-
-export default Header;
